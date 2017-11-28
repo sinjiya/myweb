96e8badcbc055ba851f906b53a99838b351d9442
sinjiya <sinjiya@umich.edu>
2017-11-28 13:12:25 -0500

    Work in progress: added external config and importing data at bootstrap if data table is empty

--- grails-app/init/myweb/BootStrap.groovy
+++ grails-app/init/myweb/BootStrap.groovy
@@ -1,9 +1,28 @@
 package myweb
 
-class BootStrap {
+import grails.util.Holders
+import grails.util.Environment
 
+class BootStrap {
+    // service injection
+    def setupService
+    def appName
     def init = { servletContext ->
+        appName = Holders.grailsApplication.config.getProperty('info.app.name')
+        log.info("system - Bootstrapping for ${appName}.")
+        log.info("system - Environment set to ${Environment.current}" )
+        def version = Holders.grailsApplication.metadata.getApplicationVersion()
+        log.info("system - App version: ${version}.")
+        //log.info("system - GRAILS_HOME: ${System.getenv('GRAILS_HOME')}.")
+        def me1 = Holders.grailsApplication.config.getProperty('dbuser')
+        log.info("system - WHO-------------: ${me1}.")
+
+        if(myweb.Chapter.count() == 0) {
+            log.info("system - No Chapter found.  Start importing from /resources.")
+            setupService.importChapters()
+        }
     }
     def destroy = {
+        log.info("system -  ${appName} Shutting down.")
     }
 }
